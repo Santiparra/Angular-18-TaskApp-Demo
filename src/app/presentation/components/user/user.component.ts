a0b5d7f81e6c7dc7f90f0887f0ff646dd0df7ce6
santiparra <122759810+Santiparra@users.noreply.github.com>
2024-07-20 11:28:44 -0300

    signal implementation

--- src/app/presentation/components/user/user.component.ts
+++ src/app/presentation/components/user/user.component.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
-
-import { DUMMY_USERS } from '../../utils/dummy-users';
+import {
+  Component,
+  computed,
+  Signal,
+  signal,
+  WritableSignal,
+} from '@angular/core';
 
 import { User } from '../../../domain/models/user.model';
+
+import { DUMMY_USERS } from '../../utils/dummy-users';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
@@ -11,17 +17,16 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   standalone: true,
   imports: [],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
 export class UserComponent {
-  selectedUser: User = DUMMY_USERS[randomIndex];
-
-  get imgAddress(): string {
-    return 'assets/users/' + this.selectedUser.avatar;
-  }
+  selectedUser: WritableSignal<User> = signal(DUMMY_USERS[randomIndex]);
+  imgAddress: Signal<string> = computed(
+    () => 'assets/users/' + this.selectedUser().avatar
+  );
 
   onSelectUser(): void {
     const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser = DUMMY_USERS[randomIndex];
+    this.selectedUser.set(DUMMY_USERS[randomIndex]);
   }
 }
