21358cabaca4bddb1b438f9d21f20124ff48d8ac
santiparra <122759810+Santiparra@users.noreply.github.com>
2024-07-20 13:15:19 -0300

    new conditional rendering sintax

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,22 +2,33 @@ import { Component } from '@angular/core';
 
 import { User } from './domain/models/user.model';
 
+import { DUMMY_USERS } from './data/dummy-users';
+
 import { HeaderComponent } from './presentation/components/header/header.component';
 import { UserComponent } from "./presentation/components/user/user.component";
-import { DUMMY_USERS } from './presentation/utils/dummy-users';
+import { TasksComponent } from "./presentation/components/tasks/tasks.component";
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Angular-18-TaskApp-Demo';
   users: User[] = DUMMY_USERS;
+  selectedUserId?: string ;
+
+  get selectedUser() {
+
+    return this.users.find(user => user.id === this.selectedUserId);
+  
+  }
 
   onSelectUser(id: string): void {
-    console.log('User selected: ' + id);
+    this.selectedUserId = id;
   }
 }
--- src/app/presentation/components/user/user.component.ts
+++ src/app/presentation/components/user/user.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input, output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+import { User } from '../../../domain/models/user.model';
 
 @Component({
   selector: 'app-user',
@@ -8,17 +10,14 @@ import { Component, Input, output } from '@angular/core';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  @Input({ required: true }) id!: string;
-  @Input({ required: true }) avatar!: string;
-  @Input({ required: true }) name!: string;
-  //@Output() selected = new EventEmitter<string>();
-  selected = output<string>();
+  @Input({ required: true }) user!: User;
+  @Output() selected = new EventEmitter<string>();
 
   get imgAddress(): string {
-    return 'assets/users/' + this.avatar;
+    return 'assets/users/' + this.user.avatar;
   }
 
   onSelectUser(): void {
-    this.selected.emit(this.id);
+    this.selected.emit(this.user.id);
   }
 }
